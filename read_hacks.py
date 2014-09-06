586b8dc5881b581a4540fa6ccf47b39bcb8220ae
Zachary Lemberg <zachlemberg@gmail.com>
2014-09-05 23:56:15 -0700

    created read_hacks.py
    python script to grab hackaday page 1 and put into the terminal for you :)

---
+++ read_hacks.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#find hackaday headlines, no metadata support
+from urllib2 import urlopen
+text = urlopen("http://hackaday.com/").read()
+head_index = []
+arti_index = []
+titles = []
+info = []
+rando = 0
+next = 0
+def subhtml(text): #subtract html from text
+	r = 0
+	while r != -1:
+		r = text.find('<')
+		if r != -1:
+			n = text.find('>')
+			text = text[0:r] + text[n+1:]
+	return text
+def substuff(text): #subtracts stupid &nbsp; or &#8212;
+	r = 0
+	while r != -1:
+		r = text.find('&')
+		if r != -1:
+			n = text.find(';',r)
+			text = text[0:r] + text[n+1:]
+	return text
+while next != -1:
+	next = text.find('<h2 class="entry-title">',next+1)
+	if next != -1:
+		head_index.append(next)
+for num in head_index:
+	rando = text.find('</h2>',num)
+	titles.append(substuff(subhtml(text[num:rando])))
+next = 0
+while next != -1:
+	next = text.find('<div class="entry-content">',next+1)
+	if next != -1:
+		arti_index.append(next)
+for start in arti_index:
+	rando = text.find('</div>',start)
+	info.append(substuff(subhtml(text[start:rando])))
+for i in range(0,len(head_index)):
+	print titles[i]
+	print info[i]
+	print " "
