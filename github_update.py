08c5f8c84ba2530c33558ec6ac4a1a22f4808aa7
Zachary Lemberg <zachlemberg@gmail.com>
2014-09-10 11:57:31 -0700

    update github_update.py
    add comments to the file

--- github_update.py
+++ github_update.py
@@ -3,7 +3,9 @@
 scrap_files = ['youtube_check.py','read_hacks.py']#files from scraping project
 #raw github url for the project code
 scrap_url = "https://raw.githubusercontent.com/zachlemberg/Scraping/master/"
-directory = '/home/zlemberg/scripts/' #where you want to put the files
+directory = '/home/example_user/scripts/' #where you want to put the files
+#if you don't want to use the directory just put, directory = ''
+#the files will be written in the same path where the github_update file is
 from urllib2 import urlopen
 for name in scrap_files:
 	text = urlopen(scrap_url + name).read()
